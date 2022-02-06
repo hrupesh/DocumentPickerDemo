ebf588293bf038ac4849100c4219aa6e716f6098
Rupesh Chaudhari <rupesh.chaudhari@bajajfinserv.in>
2022-02-07 01:26:40 +0530

    Document Picker Demo 📑🔥

---
+++ App.js
@@ -0,0 +1,51 @@
+import React, {useCallback, useState} from 'react';
+import {Button, SafeAreaView, StatusBar, StyleSheet, Text} from 'react-native';
+import DocumentPicker, {types} from 'react-native-document-picker';
+
+const App = () => {
+  const [fileResponse, setFileResponse] = useState([]);
+
+  const handleDocumentSelection = useCallback(async () => {
+    try {
+      const response = await DocumentPicker.pick({
+        presentationStyle: 'fullScreen',
+        type: [types.pdf],
+        allowMultiSelection: true,
+      });
+      setFileResponse(response);
+    } catch (err) {
+      console.warn(err);
+    }
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle={'dark-content'} />
+      {fileResponse.map((file, index) => (
+        <Text
+          key={index.toString()}
+          style={styles.uri}
+          numberOfLines={1}
+          ellipsizeMode={'middle'}>
+          {file?.uri}
+        </Text>
+      ))}
+      <Button title="Select 📑" onPress={handleDocumentSelection} />
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  uri: {
+    paddingBottom: 8,
+    paddingHorizontal: 18,
+  },
+});
